2218a51ac436c487f0697c99351c60b0a335bc15
Eric Chen (GCR CSS) <shengc@microsoft.com>
2017-04-17 10:15:40 +0800

    update extended properties.

--- AnalysisServicesProcessing/ASProcessing.cs
+++ AnalysisServicesProcessing/ASProcessing.cs
@@ -1,17 +1,17 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Microsoft.AnalysisServices.AdomdClient;
 using Microsoft.Azure.Management.DataFactories.Models;
 using Microsoft.Azure.Management.DataFactories.Runtime;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
-using System.Collections.Generic;
-using Microsoft.AnalysisServices.AdomdClient;
-using System;
 
 
 namespace AnalysisServicesProcessing
 {
-    public class ASProcessing:IDotNetActivity
+    public class ASProcessing : IDotNetActivity
     {
 
         public IDictionary<string, string> Execute(
@@ -23,8 +23,9 @@ namespace AnalysisServicesProcessing
             // get extended properties defined in activity JSON definition
             // (for example: SliceStart)
             DotNetActivity dotNetActivity = (DotNetActivity)activity.TypeProperties;
-            string sliceStartString = dotNetActivity.ExtendedProperties["SliceStart"];
-                        
+            string scriptContainer = dotNetActivity.ExtendedProperties["scriptContainer"];
+            string scriptPath = dotNetActivity.ExtendedProperties["scriptPath"];
+
             // log all extended properties            
             IDictionary<string, string> extendedProperties = dotNetActivity.ExtendedProperties;
             logger.Write("Logging extended properties if any...");
@@ -33,19 +34,19 @@ namespace AnalysisServicesProcessing
             {
                 logger.Write("<key:{0}> <value:{1}>", entry.Key, entry.Value);
             }
-            
-            //get Azure Storage Linked Service Connection String from output data set. We use this to access the TMSL script for AS processing
-            AzureStorageLinkedService outputLinkedService;                        
-            Dataset outputDataset = datasets.Single(dataset => dataset.Name == activity.Outputs.Single().Name);
-
-            AzureBlobDataset outputTypeProperties;
-            outputTypeProperties = outputDataset.Properties.TypeProperties as AzureBlobDataset;
 
             foreach (LinkedService ls in linkedServices)
                 logger.Write("linkedService.Name {0}", ls.Name);
 
             foreach (Dataset ds in datasets)
                 logger.Write("dataset.Name {0}", ds.Name);
+
+            //get Azure Storage Linked Service Connection String from output data set. We use this to access the TMSL script for AS processing
+            AzureStorageLinkedService outputLinkedService;
+            Dataset outputDataset = datasets.Single(dataset => dataset.Name == activity.Outputs.Single().Name);
+
+            AzureBlobDataset outputTypeProperties;
+            outputTypeProperties = outputDataset.Properties.TypeProperties as AzureBlobDataset;
 
             // get the  Azure Storate linked service from linkedServices object            
             outputLinkedService = linkedServices.First(
@@ -56,29 +57,32 @@ namespace AnalysisServicesProcessing
 
             // get the connection string in the linked service
             string blobconnectionString = outputLinkedService.ConnectionString;
-            // read the blob content for Analysis Services processing TMSL json script
-            string asCmdStr = ReadBlob(blobconnectionString, "container", "folder/processdatabase.json");
             
-            // Connection string to connect to Azure AS
+            
+            // read the blob content for Analysis Services processing TMSL json script from the extended properties passed to activity
+            string asCmdStr = ReadBlob(blobconnectionString, scriptContainer, scriptPath);
+            // Connection string to connect to Azure AS. User can add further logic here for security as the extended properties in ADF JSON script is general purpose and does not encrypt the string. 
             string asConnStr = @"<AS Connection String>";
-            AdomdConnection asConn = new AdomdConnection(asConnStr);
-            AdomdCommand asCmd = asConn.CreateCommand();
-            asCmd.CommandText = asCmdStr;
-
+            
             try
             {
-                asConn.Open();
-                asCmd.ExecuteNonQuery();
+                using (AdomdConnection asConn = new AdomdConnection(asConnStr))
+                {
+                    asConn.Open();
+                    AdomdCommand asCmd = asConn.CreateCommand();
+                    asCmd.CommandText = asCmdStr;
+                    asCmd.ExecuteNonQuery();
+                }
             }
             catch (Exception e)
             {
                 logger.Write(e.ToString());
-            }                 
+            }
 
             return new Dictionary<string, string>();
         }
 
-        public static string ReadBlob(string connectionString,string blobContainer, string blobPath)
+        public static string ReadBlob(string connectionString, string blobContainer, string blobPath)
         {
             CloudStorageAccount inputStorageAccount = CloudStorageAccount.Parse(connectionString);
             CloudBlobClient inputClient = inputStorageAccount.CreateCloudBlobClient();
@@ -95,6 +99,5 @@ namespace AnalysisServicesProcessing
             }
             return CmdStr;
         }
-        
     }
 }
